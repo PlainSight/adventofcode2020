86587ddc840d596154d499a845997e0db49ad0c5
Alex <alexander.ogier@gmail.com>
2023-10-27 12:14:19 +1300

    construct the picture

--- day20/part2.js
+++ day20/part2.js
@@ -5,19 +5,11 @@ var input = fs.readFileSync('./sample.txt', 'utf8').split('\r\n\r\n');
 var tiles = [];
 
 function rotateArray(ar, num) {
-     for (var n = 0; n < num; n++) {
-        var end = ar.pop();
-        ar.unshift(end);
-    }
-    return ar;
-}
-
-function flipArray(ar, dir) {
-    if (dir == 'vertically') {
-        return [ar[2], ar[1], ar[0], ar[3]];
-    } else { //horizontally
-        return [ar[0], ar[3], ar[2], ar[1]];
-    }
+    for (var n = 0; n < num; n++) {
+       var end = ar.pop();
+       ar.unshift(end);
+   }
+   return ar;
 }
 
 for(var i in input) {
@@ -25,263 +17,229 @@ for(var i in input) {
 
     var id = t[0].match(/Tile (\d+):/)[1];
 
-    var left1 = 0;
-    var left2 = 0;
-    var right1 = 0;
-    var right2 = 0;
-    var top1 = 0;
-    var top2 = 0;
-    var bottom1 = 0;
-    var bottom2 = 0;
+    var len = t.length-2;
 
-    var data = [];
+    var ss = [
+        [0, 1],
+        [len, 1],
+        [len, len+1],
+        [0, len+1]
+    ];
 
-    // number all sides with least significant digits to the left;
+    var es = [
+        [len, 1],
+        [len, len+1],
+        [0, len+1],
+        [0, 1]
+    ];
 
-    for(var y = 1; y < t.length; y++) {
+    var ds = [
+        [1, 0],
+        [0, 1],
+        [-1, 0],
+        [0, -1]
+    ];
 
-        var rd = y-1;
-        var ld = t.length-y-1;
+    var borders = [];
+    var bordersPolarity = [];
 
-        if (y >= 2 && y <= 9) {
-            data.push(t[y].split('').slice(1, 9));
+    data = t.slice(2, 10).map(s => s.split('').slice(1, 9));
+
+    ss.forEach((sss, ssi) => {
+        var x = sss[0];
+        var y = sss[1];
+
+        var d = 0;
+        var dd = len;
+
+        var one = (t[y][x] == '#' ? 1 : 0) * Math.pow(2, d);
+        var two = (t[y][x] == '#' ? 1 : 0) * Math.pow(2, dd);
+
+        while (x != es[ssi][0] || y != es[ssi][1]) {
+            x += ds[ssi][0];
+            y += ds[ssi][1];
+            d++;
+            dd--;
+            one += (t[y][x] == '#' ? 1 : 0) * Math.pow(2, d);
+            two += (t[y][x] == '#' ? 1 : 0) * Math.pow(2, dd);
         }
 
-        if (t[y][t[y].length-1] == '#') {
-            right1 += Math.pow(2, rd);
-            right2 += Math.pow(2, ld);
-        }
-
-        if (t[y][0] == '#') {
-            left1 += Math.pow(2, rd);
-            left2 += Math.pow(2, ld);
-        }
-
-        if (y == 1) {
-            var d = 0;
-            for(var x = 0; x < t[y].length; x++) {
-                if (t[y][x] == '#') {
-                    top1 += Math.pow(2, d);
-                }
-                d++;
-            }
-            d = 0;
-            for(var x = t[y].length-1; x >= 0; x--) {
-                if (t[y][x] == '#') {
-                    top2 += Math.pow(2, d);
-                }
-                d++;
-            }
-        }
-
-        if (y == t.length - 1) {
-            var d = 0;
-            for(var x = t[y].length-1; x >= 0; x--) {
-                if (t[y][x] == '#') {
-                    bottom1 += Math.pow(2, d);
-                }
-                d++;
-            }
-            d = 0;
-            for(var x = 0; x < t[y].length; x++) {
-                if (t[y][x] == '#') {
-                    bottom2 += Math.pow(2, d);
-                }
-                d++;
-            }
-        }
-    }
+        borders.push(Math.max(one, two));
+        bordersPolarity.push(one > two);
+    });
 
     var tile = {
         id: id,
-        borders: [Math.max(top1, top2), Math.max(right1, right2), Math.max(bottom1, bottom2), Math.max(left1, left2)],
-        borderedBy: [false, false, false, false],
-        requiredNeighbours: 0,
+        borders: borders,
+        bordersPolarity: bordersPolarity,
         x: 0,
         y: 0,
+        rotation: 0,    //clockwise always
+        flipped: false,
         placed: false,
-        rotationRequired: 0,
+        order: 0,
         data: data
     };
 
     tiles.push(tile);
 }
 
-var bordersByShared = {};
+var tilesByBorder = tiles.reduce((a, c) => {
+    c.borders.forEach(b => {
+        a[b] = (a[b] || []);
+        a[b].push(c);
+    });
+    return a;
+}, {});
 
-for (var t in tiles) {
-    var tile = tiles[t];
+function borderToCheck(b) {
+    return tilesByBorder[b] && tilesByBorder[b].length > 1;
+}
 
-    for (var b in tile.borders) {
-        var border = tile.borders[b];
+function getNeighbour(tile, border) {
+    return tilesByBorder[border].filter(t => t != tile)[0];
+}
 
-        if (!bordersByShared[border]) {
-            bordersByShared[border] = 1;
-        } else {
-            bordersByShared[border]++;
-        }
+function polarityOfBorder(tile, border) {
+    var index = tile.borders.indexOf(border);
+    return tile.bordersPolarity[index];
+}
+
+function calculateFlip(tile1, tile2, border) {
+    var polarity1 = polarityOfBorder(tile1, border);
+    var polarity2 = polarityOfBorder(tile2, border);
+    if (polarity1 == polarity2) {
+        // flip
+        tile2.flipped = true;
+        tile2.bordersPolarity = tile2.bordersPolarity.map(a => !a);
+
+        var bpt = tile2.bordersPolarity[2];
+        tile2.bordersPolarity[2] = tile2.bordersPolarity[0];
+        tile2.bordersPolarity[0] = bpt;
+
+        var bt = tile2.borders[2];
+        tile2.borders[2] = tile2.borders[0];
+        tile2.borders[0] = bt;
+
+        console.log('flipping tile ', tile2.order, tile2.id);
+    }
+} 
+
+function calculateRotate(tile1, tile2, border) {
+    var index1 = tile1.borders.indexOf(border);
+    var index2 = tile2.borders.indexOf(border);
+    var rotate = ((6 + index1) - index2) % 4;
+
+    tile2.rotation = rotate;
+    if (rotate != 0) {
+        tile2.borders = rotateArray(tile2.borders, rotate);
+        tile2.bordersPolarity = rotateArray(tile2.bordersPolarity, rotate);
+        console.log('rotating tile ', tile2.order, tile2.id);
     }
 }
 
-for (var t in tiles) {
-    var tile = tiles[t];
+var tilesWithTwoBorders = tiles.filter(t => t.borders.filter(b => borderToCheck(b)).length == 2);
 
-    var sharedBorders = 0;
+console.log(tilesWithTwoBorders.length);
 
-    for (var b in tile.borders) {
-        var border = tile.borders[b];
+var queue = [tilesWithTwoBorders[0]];
 
-        sharedBorders += (bordersByShared[border] == 2 ? 1 : 0);
-    }
+tilesWithTwoBorders[0].x = 0;
+tilesWithTwoBorders[0].y = 0;
+tilesWithTwoBorders[0].placed = true;
 
-    tile.requiredNeighbours = sharedBorders;
-}
+var ds = [
+    [0, 1],
+    [1, 0],
+    [0, -1],
+    [-1, 0]
+];
 
-//console.log(tiles);
+var order = 1;
 
-outer: while(tiles.filter(t => !t.placed).length > 0) {
-    var placedTiles = tiles.filter(t => t.placed);
-    var unplacedTiles = tiles.filter(t => !t.placed);
+while(queue.length) {
+    var currentTile = queue.pop();
 
-    if (placedTiles.length == 0) {
-        tiles[0].placed = true;
-        //console.log('placing tile', tiles[0]);
-    } else {
-        var toHaveNeighbour = placedTiles.filter(pt => pt.borderedBy.filter(b => b).length < pt.requiredNeighbours)[0];
-
-        //console.log('get neighbour for', toHaveNeighbour);
-
-        var borderIds = toHaveNeighbour.borders.map((v, i) => { return { v: v, i: i }}).filter(v => toHaveNeighbour.borderedBy[v.i] == false);
-
-        for (var t in unplacedTiles) {
-            var tile = unplacedTiles[t];
-
-            for (var b in borderIds) {
-                var borderId = borderIds[b].v;
-                var borderIndex = borderIds[b].i; // index of border in placed tile
-
-                //console.log('ee', borderId, borderIndex);
-
-                if (tile.borders.includes(borderId)) {
-                    var indexOfBorder = tile.borders.indexOf(borderId); // index of border in unplaced tile
-
-                    var desiredIndexOfBorder = (borderIndex + 2) % 4
-
-                    var rotationRequired = (desiredIndexOfBorder + 4 - indexOfBorder) % 4;
-
-                    tile.borders = rotateArray(tile.borders, rotationRequired);
-                    tile.borderedBy = rotateArray(tile.borderedBy, rotationRequired);
-                    tile.rotationRequired = rotationRequired;
-
-                    tile.borderedBy[desiredIndexOfBorder] = true;
-
-                    toHaveNeighbour.borderedBy[borderIndex] = true;
-
-                    tile.placed = true;
-                    switch (desiredIndexOfBorder) {
-                        case 0:
-                            tile.x = toHaveNeighbour.x;
-                            tile.y = toHaveNeighbour.y-1;
-                        break;
-                        case 1:
-                            tile.x = toHaveNeighbour.x+1;
-                            tile.y = toHaveNeighbour.y;
-                        break;
-                        case 2:
-                            tile.x = toHaveNeighbour.x;
-                            tile.y = toHaveNeighbour.y+1;
-                        break;
-                        case 3:
-                            tile.x = toHaveNeighbour.x-1;
-                            tile.y = toHaveNeighbour.y;
-                        break;
-                    }
-                    //console.log('placing tile', placedTiles.length +1, tile);
-                    continue outer;
-                }
+    currentTile.borders.forEach((b, bi) => {
+        if (borderToCheck(b)) {
+            var neighbour = getNeighbour(currentTile, b);
+            if (!neighbour.placed) {
+                neighbour.order = order;
+                calculateFlip(currentTile, neighbour, b);
+                calculateRotate(currentTile, neighbour, b);
+                delete tilesByBorder[b];
+                neighbour.placed = true;
+                neighbour.x = currentTile.x + ds[bi][0];
+                neighbour.y = currentTile.y + ds[bi][1];
+                order++;
+                queue.push(neighbour);
             }
         }
-        // already placed
-        toHaveNeighbour.borderedBy[0] = true;
-        toHaveNeighbour.borderedBy[1] = true;
-        toHaveNeighbour.borderedBy[2] = true;
-        toHaveNeighbour.borderedBy[3] = true;
-    }
+    })
 }
 
-// construct picture
+var minx = Math.abs(Math.min(...tiles.map(t => t.x)));
+var miny = Math.abs(Math.min(...tiles.map(t => t.y)));
 
-var minx = Math.min(...tiles.map(t => t.x));
-var miny = Math.min(...tiles.map(t => t.y));
+tiles.forEach(t => {
+    t.x += minx;
+    t.y += miny;
+});
+
+minx = Math.abs(Math.min(...tiles.map(t => t.x)));
+miny = Math.abs(Math.min(...tiles.map(t => t.y)));
 var maxx = Math.max(...tiles.map(t => t.x));
 var maxy = Math.max(...tiles.map(t => t.y));
 
-console.log(minx, miny, maxx, maxy);
+var tdim = tiles[0].data.length - 1;
 
-var picture = new Array(64 * tiles.length);
+console.log(tdim);
 
-function destIndex(mx, my, x, y, mul) {
-    var rx = (mx*8) + x;
-    var ry = (my*8) + y;
+function getAt(tile, x, y) {
+    var ss = [[0, 0], [tdim, 0], [tdim, tdim], [0, tdim]];
+    var ssd = [
+        [[1, 0], [0, 1]], 
+        [[0, -1], [1, 0]], 
+        [[-1, 0], [0, -1]], 
+        [[0, 1], [-1, 0]]
+    ];
 
-    return (ry * mul) + rx
-}
+    var xx = ss[tile.rotation][0] + (ssd[tile.rotation][0][0]*x) + (ssd[tile.rotation][0][1]*y);
+    var yy = ss[tile.rotation][1] + (ssd[tile.rotation][1][0]*x) + (ssd[tile.rotation][1][1]*y);
 
-var picdim = 8*(1+maxx-minx);
-
-for(var t in tiles) {
-    var tile = tiles[t];
-
-    console.log(tile);
-
-    var minxdest = tile.x - minx;
-    var minydest = tile.y - miny;
-
-    var destx = 0;
-    var desty = 0;
-    switch (tile.rotationRequired) {
-        case 0:
-            for (var y = 0; y < 8; y++) {
-                destx = 0;
-                for (var x = 0; x < 8; x++) {
-                    picture[destIndex(minxdest, minydest, destx, desty, picdim)] = tile.data[y][x];
-                    destx++;
-                }
-                desty++;
-            }
-            break;
-        case 1:
-            for (var x = 0; x < 8; x++) {
-                destx = 0;
-                for (var y = 7; y >= 0; y--) {
-                    picture[destIndex(minxdest, minydest, destx, desty, picdim)] = tile.data[y][x];
-                    destx++;
-                }
-                desty++;
-            }
-            break;
-        case 2:
-            for (var x = 7; x >= 0; x--) {
-                destx = 0;
-                for (var y = 7; y >= 0; y--) {
-                    picture[destIndex(minxdest, minydest, destx, desty, picdim)] = tile.data[y][x];
-                    destx++;
-                }
-                desty++;
-            }
-            break;
-        case 3:
-
-
-
-            break;
+    if (tile.flipped) {
+        yy = tdim - yy;
     }
+
+    //console.log('g', yy, xx);
+
+    return tile.data[yy][xx];
 }
 
+console.log(`${minx} -- ${maxy}`);
 
+for(var y = maxy; y >= miny; y--) {
+    var str = `${y} `;
+    for(var x = minx; x <= maxx; x++) {
+        var ct = tiles.filter(t => t.x == x && t.y == y)[0];
+        if (ct) {
+            str += (ct.order%10);
+        } else {
+            str += '.';
+        }
+    }
+    console.log(str);
+}
 
-console.log(picdim);
-
-for (var y = 0; y < picdim; y++) {
-    console.log(picture.slice(picdim*y, picdim*(y+1)).join(''));
+for (var i = 0; i < 8; i++) {
+    tiles[0].rotation = i % 4;
+    tiles[0].flipped = (i / 2) >= 2;
+    console.log('r', tiles[0].rotation, tiles[0].flipped);
+    for (var y = 0; y <= tdim; y++) {
+        var str = '';
+        for(var x = 0; x <= tdim; x++) {
+            str += getAt(tiles[0], x, y);
+        }
+        console.log(str);
+    }
 }
