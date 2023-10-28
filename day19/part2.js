01ed129f6f9ca510eebca4a3c2bfa13026404e8c
Alex <alexander.ogier@gmail.com>
2023-10-28 15:39:52 +1300

    finally complete day19

--- day19/part2.js
+++ day19/part2.js
@@ -1,3 +1,61 @@
 var fs = require('fs');
 
-var input = fs.readFileSync('./input.txt', 'utf8').split('\r\n');
+var input = fs.readFileSync('./input.txt', 'utf8').split('\r\n\r\n');
+
+var rawRules = input[0].split('\r\n');
+var messages = input[1].split('\r\n');
+
+var rules = {};
+
+for (var r in rawRules) {
+    var rule = rawRules[r].split(':');
+
+    var ruleId = parseInt(rule[0]);
+    var ruleParts = rule[1].trim().split('|').map(m => m.trim().split(' ').map(n => n.includes('"') ? n.replace(/"/g, '') : parseInt(n)));
+
+    rules[ruleId] = ruleParts;
+}
+
+rules[8] = [ [ 42 ], [ 42, 8 ] ];
+rules[11] = [ [ 42, 31 ], [ 42, 11, 31 ] ];
+
+function checkRule(id, string, index) {
+    var rule = rules[id];
+    //console.log('checking rule ', rule, ' on ', string, 'at', index);
+    var successes = [];
+outer: for(var rr = 0; rr < rule.length; rr++) {
+        var option = rule[rr];
+        var lis = [ index ];
+
+        for (var c = 0; c < option.length; c++) {
+            if (lis.length == 0) {
+                continue outer;
+            }
+            var newlis = [];
+            lis.forEach(li => {
+                var cond = option[c];
+                if (/[ab]/.test(cond)) {
+                    if (string[li] == cond) {
+                        newlis.push(li+1);
+                    }
+                } else {
+                    var res = checkRule(cond, string, li);
+                    newlis.push(...res);
+                }
+            });
+            lis = newlis;
+        }
+
+        successes.push(...lis);
+    }
+
+    return successes;
+}
+
+var goodMessages = 0;
+
+messages.forEach(m => {
+    goodMessages += checkRule(0, m, 0).includes(m.length) ? 1 : 0;
+});
+
+console.log(goodMessages);
